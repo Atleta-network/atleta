be408cedc91a0ee81705a182074d61a0303f33aa
Ales Tsurko <ales.tsurko@gmail.com>
2023-12-08 14:51:15 +0300

    Configure CI

--- node/src/client.rs
+++ node/src/client.rs
@@ -10,7 +10,7 @@ use crate::eth::EthCompatRuntimeApiCollection;
 pub type FullBackend = sc_service::TFullBackend<Block>;
 /// Full client.
 pub type FullClient<RuntimeApi, Executor> =
-	sc_service::TFullClient<Block, RuntimeApi, NativeElseWasmExecutor<Executor>>;
+    sc_service::TFullClient<Block, RuntimeApi, NativeElseWasmExecutor<Executor>>;
 
 pub type Client = FullClient<sportchain_runtime::RuntimeApi, TemplateRuntimeExecutor>;
 
@@ -23,55 +23,55 @@ pub type HostFunctions = ();
 
 pub struct TemplateRuntimeExecutor;
 impl NativeExecutionDispatch for TemplateRuntimeExecutor {
-	type ExtendHostFunctions = HostFunctions;
+    type ExtendHostFunctions = HostFunctions;
 
-	fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
-		sportchain_runtime::api::dispatch(method, data)
-	}
+    fn dispatch(method: &str, data: &[u8]) -> Option<Vec<u8>> {
+        sportchain_runtime::api::dispatch(method, data)
+    }
 
-	fn native_version() -> NativeVersion {
-		sportchain_runtime::native_version()
-	}
+    fn native_version() -> NativeVersion {
+        sportchain_runtime::native_version()
+    }
 }
 
 /// A set of APIs that every runtimes must implement.
 pub trait BaseRuntimeApiCollection:
-	sp_api::ApiExt<Block>
-	+ sp_api::Metadata<Block>
-	+ sp_block_builder::BlockBuilder<Block>
-	+ sp_offchain::OffchainWorkerApi<Block>
-	+ sp_session::SessionKeys<Block>
-	+ sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
+    sp_api::ApiExt<Block>
+    + sp_api::Metadata<Block>
+    + sp_block_builder::BlockBuilder<Block>
+    + sp_offchain::OffchainWorkerApi<Block>
+    + sp_session::SessionKeys<Block>
+    + sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
 {
 }
 
 impl<Api> BaseRuntimeApiCollection for Api where
-	Api: sp_api::ApiExt<Block>
-		+ sp_api::Metadata<Block>
-		+ sp_block_builder::BlockBuilder<Block>
-		+ sp_offchain::OffchainWorkerApi<Block>
-		+ sp_session::SessionKeys<Block>
-		+ sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
+    Api: sp_api::ApiExt<Block>
+        + sp_api::Metadata<Block>
+        + sp_block_builder::BlockBuilder<Block>
+        + sp_offchain::OffchainWorkerApi<Block>
+        + sp_session::SessionKeys<Block>
+        + sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block>
 {
 }
 
 /// A set of APIs that template runtime must implement.
 pub trait RuntimeApiCollection:
-	BaseRuntimeApiCollection
-	+ EthCompatRuntimeApiCollection
-	+ sp_consensus_aura::AuraApi<Block, AuraId>
-	+ sp_consensus_grandpa::GrandpaApi<Block>
-	+ frame_system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Nonce>
-	+ pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
+    BaseRuntimeApiCollection
+    + EthCompatRuntimeApiCollection
+    + sp_consensus_aura::AuraApi<Block, AuraId>
+    + sp_consensus_grandpa::GrandpaApi<Block>
+    + frame_system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Nonce>
+    + pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
 {
 }
 
 impl<Api> RuntimeApiCollection for Api where
-	Api: BaseRuntimeApiCollection
-		+ EthCompatRuntimeApiCollection
-		+ sp_consensus_aura::AuraApi<Block, AuraId>
-		+ sp_consensus_grandpa::GrandpaApi<Block>
-		+ frame_system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Nonce>
-		+ pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
+    Api: BaseRuntimeApiCollection
+        + EthCompatRuntimeApiCollection
+        + sp_consensus_aura::AuraApi<Block, AuraId>
+        + sp_consensus_grandpa::GrandpaApi<Block>
+        + frame_system_rpc_runtime_api::AccountNonceApi<Block, AccountId, Nonce>
+        + pallet_transaction_payment_rpc_runtime_api::TransactionPaymentApi<Block, Balance>
 {
 }
