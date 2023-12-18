15c3b64d415c1c69cf54013c0fdadbb9a9894840
Ales Tsurko <ales.tsurko@gmail.com>
2023-12-18 14:26:39 +0300

    Fix number of digits in the native currency

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -245,8 +245,8 @@ fn testnet_genesis(
 
         // Monetary
         balances: BalancesConfig {
-            // Configure endowed accounts with initial balance of 1 << 60.
-            balances: endowed_accounts.iter().cloned().map(|k| (k, 1 << 60)).collect(),
+            // Configure endowed accounts with initial balance
+            balances: endowed_accounts.iter().cloned().map(|k| (k, ENDOWMENT)).collect(),
         },
         transaction_payment: Default::default(),
 
