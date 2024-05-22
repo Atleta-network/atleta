935bcf64cf4d5ecdac6302aa8fd7116413ecf809
Ales Tsurko <ales.tsurko@gmail.com>
2024-05-22 14:14:40 +0300

    Add dispatch precompile call filter

--- runtime/src/precompiles.rs
+++ runtime/src/precompiles.rs
@@ -1,16 +1,17 @@
-use frame_support::dispatch::{GetDispatchInfo, PostDispatchInfo};
+use frame_support::dispatch::{GetDispatchInfo, Pays};
 use pallet_evm::{
     IsPrecompileResult, Precompile, PrecompileHandle, PrecompileResult, PrecompileSet,
 };
-use parity_scale_codec::Decode;
 use sp_core::H160;
-use sp_runtime::traits::Dispatchable;
 use sp_std::marker::PhantomData;
 
-use pallet_evm_precompile_dispatch::Dispatch;
+use pallet_evm::ExitError;
+use pallet_evm_precompile_dispatch::{Dispatch, DispatchValidateT};
 use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
+
+use crate::*;
 
 pub struct FrontierPrecompiles<R>(PhantomData<R>);
 
@@ -22,14 +23,12 @@ where
         Self(Default::default())
     }
     pub fn used_addresses() -> [H160; 8] {
-        [hash(1), hash(2), hash(3), hash(4), hash(5), hash(6), hash(1024), hash(1025)]
+        [hash(1), hash(2), hash(3), hash(4), hash(5), hash(1024), hash(1025), hash(1026)]
     }
 }
 impl<R> PrecompileSet for FrontierPrecompiles<R>
 where
     R: pallet_evm::Config,
-    R::RuntimeCall: Dispatchable<PostInfo = PostDispatchInfo> + GetDispatchInfo + Decode,
-    <R::RuntimeCall as Dispatchable>::RuntimeOrigin: From<Option<R::AccountId>>,
 {
     fn execute(&self, handle: &mut impl PrecompileHandle) -> Option<PrecompileResult> {
         match handle.code_address() {
@@ -39,10 +38,10 @@ where
             a if a == hash(3) => Some(Ripemd160::execute(handle)),
             a if a == hash(4) => Some(Identity::execute(handle)),
             a if a == hash(5) => Some(Modexp::execute(handle)),
-            a if a == hash(6) => Some(Dispatch::<R>::execute(handle)),
             // Non-Frontier specific nor Ethereum precompiles :
             a if a == hash(1024) => Some(Sha3FIPS256::execute(handle)),
             a if a == hash(1025) => Some(ECRecoverPublicKey::execute(handle)),
+            a if a == hash(1026) => Some(Dispatch::<Runtime, DispatchCallFilter>::execute(handle)),
             _ => None,
         }
     }
@@ -57,4 +56,37 @@ where
 
 fn hash(a: u64) -> H160 {
     H160::from_low_u64_be(a)
+}
+
+struct DispatchCallFilter;
+
+impl DispatchValidateT<AccountId, RuntimeCall> for DispatchCallFilter {
+    fn validate_before_dispatch(
+        _origin: &AccountId,
+        call: &RuntimeCall,
+    ) -> Option<fp_evm::PrecompileFailure> {
+        let info = call.get_dispatch_info();
+
+        if matches!(
+            call,
+            // we ALLOW dispatching these calls
+            RuntimeCall::Staking(..)
+                | RuntimeCall::Democracy(..)
+                | RuntimeCall::Elections(..)
+                | RuntimeCall::Preimage(..)
+                | RuntimeCall::NominationPools(..)
+        ) {
+            None
+        } else if info.pays_fee == Pays::No || info.class == DispatchClass::Mandatory {
+            Some(fp_evm::PrecompileFailure::Error {
+                exit_status: ExitError::Other("Permission denied calls".into()),
+            })
+        } else {
+            Some(fp_evm::PrecompileFailure::Error {
+                exit_status: ExitError::Other(
+                    "The call is not allowed to be dispatched via precompile.".into(),
+                ),
+            })
+        }
+    }
 }
