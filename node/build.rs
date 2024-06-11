946602d08182f04054c7e3d57b90e6f57c76ddb3
hrls <viktor.kharitonovich@gmail.com>
2024-06-11 17:53:08 +0300

    Deny missing docs

--- node/build.rs
+++ node/build.rs
@@ -1,3 +1,5 @@
+//!#[allow(missing_docs)]
+
 use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
 fn main() {
--- runtime/build.rs
+++ runtime/build.rs
@@ -1,3 +1,5 @@
+//!#[allow(missing_docs)]
+
 fn main() {
     #[cfg(feature = "std")]
     substrate_wasm_builder::WasmBuilder::new()
