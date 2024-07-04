febb7641f61a5e6b430839971765a6ed5e25e12f
Ales Tsurko <ales.tsurko@gmail.com>
2024-07-04 17:11:41 +0300

    Bump runtime version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -170,10 +170,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("atleta"),
     impl_name: create_runtime_str!("atleta"),
     authoring_version: 1,
-    spec_version: 1,
+    spec_version: 4,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 1,
+    transaction_version: 2,
     state_version: 1,
 };
 
