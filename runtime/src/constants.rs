02c429355882eb65d4264b03e8d3d0158eb08dee
hrls <viktor.kharitonovich@gmail.com>
2024-09-25 17:12:52 +0300

    chore: add todo and trigger ci

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -6,7 +6,7 @@ pub mod currency {
 
     pub const MILLICENTS: Balance = 10_000_000_000_000;
     pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
-    pub const DOLLARS: Balance = 100 * CENTS;
+    pub const DOLLARS: Balance = 100 * CENTS; // TODO: rename it to ATLA
 
     pub const fn deposit(items: u32, bytes: u32) -> Balance {
         items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
