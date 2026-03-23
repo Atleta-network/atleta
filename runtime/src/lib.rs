818e78bc52a6a18aa90e7bd0db050bdf7a7193ed
Dzmitry Rybakou <dmitri.safe@gmail.com>
2026-03-23 11:52:10 +0300

    chore(runtime): bump spec_version 110 → 111 for debug trace upgrade

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -213,7 +213,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("atleta"),
     impl_name: create_runtime_str!("atleta"),
     authoring_version: 1,
-    spec_version: 110,
+    spec_version: 111,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 3,
